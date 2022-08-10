1a4cee7190c9d6c2cf84c8bac76a70a093320743
drishtiagarwal2000 <drishtiag2000@gmail.com>
2022-08-10 16:32:19 +0530

    New Commit

---
+++ JAVA Course/src/testing/Calculator.java
@@ -0,0 +1,21 @@
+package testing;
+
+public class Calculator {
+	public int fact(int a) {
+		int f=1;
+		for(int i=1; i<=a; i++) {
+			f=f*i;
+		
+			
+		}
+		return f;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
